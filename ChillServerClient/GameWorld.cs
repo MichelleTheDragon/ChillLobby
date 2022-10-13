8b1123fc93e8532997e886badd933ff6ffcff788
KellanyStudent <94450265+KellanyStudent@users.noreply.github.com>
2022-10-13 11:44:28 +0200

    Layered backgrounds
    Kinda readied for a player too

--- ChillServerClient/GameWorld.cs
+++ ChillServerClient/GameWorld.cs
@@ -3,19 +3,30 @@ using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using System.Net.Sockets;
 using System.Net;
+using System.Collections.Generic;
+using ChillServerClient.World;
 
 namespace ChillServerClient
 {
     public class GameWorld : Game
     {
+        #region properties
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
+        //Server
         private ConnectionToServer myConnection;
+        //Background
         private Texture2D background;
         private Texture2D garden;
         private Texture2D tables;
-        public Vector2 speed;
+        private List<LayerdBackgrounds> _backgroundList;   //List for the background effect.
+        private List<Components> _gameComponents;           //omponents in the game
 
+        public Vector2 speed;
+        //Resolution
+        public const int WantedWidth = 1600;    //The width I want for the window
+        public const int WantedHeight = 900;    ///The height I want for the window
+        #endregion
 
         public GameWorld()
         {
@@ -35,7 +46,7 @@ namespace ChillServerClient
             Window.AllowUserResizing = true;                    //Allows users to be able to drag the window to preferred size.
             _graphics.PreferredBackBufferWidth = 1600;          //Width of the window
             _graphics.PreferredBackBufferHeight = 900;          //Height of the window
-            _graphics.SynchronizeWithVerticalRetrace = true;    //Enable VSync.
+            //_graphics.SynchronizeWithVerticalRetrace = true;    //Enable VSync.
             _graphics.ApplyChanges();                           //Needed for the resolution to change
 
             base.Initialize();
@@ -46,35 +57,69 @@ namespace ChillServerClient
             _spriteBatch = new SpriteBatch(GraphicsDevice);
 
             //Asset layers
-            background = Content.Load<Texture2D>("Backgrounds\\FuldBgwMat");    //Floor and walls
-            tables = Content.Load<Texture2D>("Backgrounds\\Tabled");            //Tables
-            garden = Content.Load<Texture2D>("Backgrounds\\Tabled");            //Garden
+            //background = Content.Load<Texture2D>("Backgrounds\\FuldBgwMat");    //Floor and walls
+            //tables = Content.Load<Texture2D>("Backgrounds\\Tabled");            //Tables
+            //garden = Content.Load<Texture2D>("Backgrounds\\Tabled");            //Garden
 
             //Server Connection
             //string hostName = Dns.GetHostName()
             //string myIP = Dns.GetHostEntry(hostName).AddressList[3].ToString();
             //myConnection = new ConnectionToServer("87.49.251.155");// "192.168.1.75");
-            // TODO: use this.Content to load your game content here
+
+            /// <summary>
+            /// Backgrounds
+            /// (ontent.Load<Texture2D>("Backgrounds\\Tabled"),_player, 0f, true)
+            /// Texture = The texture sprite
+            /// _player = if player affects the sprite (moves it by position - we dont use it)
+            /// 0f      = 0 means layer is not moving by itself (scroling effect) it is also its speed. set it to 0.1 and itll move slowly to the left
+            /// true    = Layer would be constant, repeating once it moved all the way to the left, itll reappear.
+            /// Layer = the layer it lies on. I put 0.5 as standard.
+            /// </summary>
+            _backgroundList = new List<LayerdBackgrounds>()    //the list and this is what will be in it
+            {
+                 new LayerdBackgrounds(Content.Load<Texture2D>("Backgrounds\\FuldBgwMat"),  0f) //not moving //If player affects it, player property is put before the float
+                {
+                    Layer = 0.50f,  //The layer it lies on
+                },
+                new LayerdBackgrounds(Content.Load<Texture2D>("Backgrounds\\Tabled"), 0f)  //the moving light background
+                {
+                    Layer = 0.51f,  //The layer it lies on
+                }
+            };
+            //Dummy player code til når player er sat ind, kan self rettes til
+            //_player = new Player(PLAYERTEXTURE2D)
+            //{
+            //    Position = new Vector2(50, 50),
+            //    Layerd = 0.60f; 
+            //};
         }
 
         protected override void Update(GameTime gameTime)
         {
+            
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
-
-            // TODO: Add your update logic here
+            //background
+            foreach (var bgl in _backgroundList)    //Background loop = bgl
+            {
+                bgl.Update(gameTime);
+            }
 
             base.Update(gameTime);
         }
 
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.DarkSeaGreen);
-            _spriteBatch.Begin();
-            _spriteBatch.Draw(background, new Vector2(0, 0), Color.White);
-            // TODO: Add your drawing code here
-            _spriteBatch.End();
+            GraphicsDevice.Clear(Color.DarkSeaGreen);       //Default background
+            _spriteBatch.Begin(SpriteSortMode.FrontToBack); //the order in which the sprites for the background are. front first then the other lies behind it
+            foreach (var bg in _backgroundList)             //bg = background
+            {
+                bg.Draw(gameTime, _spriteBatch);
+            }
+                //_spriteBatch.Draw(background, new Vector2(0, 0), Color.White);
+                // TODO: Add your drawing code here
+                _spriteBatch.End();
             base.Draw(gameTime);
         }
     }
